c6540908db5bb87600e638caed98daef69a1b2d5
alexkozminski <102160228+alexkozminski@users.noreply.github.com>
2023-02-21 00:06:10 +0200

    Ping fixed.

--- assets/common/help/ping.go
+++ assets/common/help/ping.go
@@ -7,15 +7,21 @@ import (
 	"time"
 )
 
-// The constant 'maxRetries' is used to set a limit to the number of times a program can attempt to execute a particular
-// task before giving up and returning an error message. This value can help ensure that the program does not get stuck
-// in an infinite loop of retries, which could cause the program to slow down or crash.
-const maxRetries = 5
-
-// Ping - The purpose of the function Ping() is to determine whether or not a remote procedure call (RPC) is available. The
-// function takes a string argument, which is the address of the RPC, and returns a boolean, indicating whether or not
+// Ping - The purpose of the function Ping() is to determine whether a remote procedure call (RPC) is available. The
+// function takes a string argument, which is the address of the RPC, and returns a boolean, indicating whether
 // the RPC is available.
 func Ping(rpc string) bool {
+
+	// The purpose of the code above is to declare a variable named 'timeout' of type 'time.Duration'. This variable is used
+	// to store the amount of time that is used to set a timeout for a certain operation.
+	var (
+		timeout time.Duration
+	)
+
+	// The purpose of this statement is to set a timeout period of 15 seconds. The timeout period is the maximum amount of
+	// time that can elapse before a certain action will be taken. In this case, the action to be taken is defined by the
+	// code that follows this statement.
+	timeout = time.Second * 15
 
 	// This code snippet is used to parse a URL from the string 'rpc' and store the result in the variable u. If an error
 	// occurs when parsing the URL, the code returns false.
@@ -28,7 +34,7 @@ func Ping(rpc string) bool {
 	// https") and then attempts to establish a connection to the host of the URL. If it is able to establish a connection,
 	// the URL is valid, and the code will return true. If it is not able to establish a connection, the code will return false.
 	if u.Scheme == "http" || u.Scheme == "https" {
-		if conn, err := retryDial(u.Host, time.Second*15); err != nil {
+		if conn, err := retryDial(u.Host, timeout); err != nil {
 			return false
 		} else {
 			conn.Close()
@@ -47,7 +53,7 @@ func Ping(rpc string) bool {
 	// The purpose of this code is to check if a connection can be established with a specified host and port, with a given
 	// timeout. It uses the retryDial() function to attempt to make the connection, and if successful, closes the connection
 	// and returns true. If the connection fails, it returns false.
-	if conn, err := retryDial(net.JoinHostPort(addr[1], addr[2]), time.Second*15); err != nil {
+	if conn, err := retryDial(net.JoinHostPort(addr[1], addr[2]), timeout); err != nil {
 		return false
 	} else {
 		conn.Close()
@@ -67,18 +73,12 @@ func retryDial(addr string, timeout time.Duration) (net.Conn, error) {
 		err  error
 	)
 
-	// The purpose of this code is to set up a loop that will run a maximum of maxRetries times. The loop will start at the
-	// value of 0, and increment the value of i by 1 each time the loop runs, and will stop running when i is equal to maxRetries.
-	for i := 0; i < maxRetries; i++ {
-		time.Sleep(timeout)
-
-		// This code is attempting to establish a network connection with an address (addr) using the TCP protocol, with a
-		// timeout of timeout seconds. The connection is stored in the conn variable and any errors are stored in the err
-		// variable. If there is no error, the loop is broken.
-		conn, err = net.DialTimeout("tcp", addr, timeout)
-		if err == nil {
-			break
-		}
+	// This code is attempting to establish a network connection with an address (addr) using the TCP protocol, with a
+	// timeout of timeout seconds. The connection is stored in the conn variable and any errors are stored in the err
+	// variable. If there is no error, the loop is broken.
+	conn, err = net.DialTimeout("tcp", addr, timeout)
+	if err == nil {
+		return conn, err
 	}
 
 	return conn, err
