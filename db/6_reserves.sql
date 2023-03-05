6d1f503a262c1954bc97621b9c837c696eb33bd3
alexkozminski <102160228+alexkozminski@users.noreply.github.com>
2023-03-05 14:22:08 +0200

    Reserves db added column.

--- db/6_reserves.sql
+++ db/6_reserves.sql
@@ -3,15 +3,15 @@ create table if not exists public.reserves
     id       serial
         constraint reserves_pk
             primary key
-        constraint reserves_id_key
-            unique,
+        unique,
     user_id  integer,
     address  varchar,
     symbol   varchar,
     platform integer,
     protocol integer,
     value    numeric(32, 18) default 0.000000000000000000 not null,
-    lock     boolean         default false              not null
+    reverse  numeric(32, 18) default 0.000000000000000000 not null,
+    lock     boolean         default false                not null
 );
 
 alter table public.reserves
